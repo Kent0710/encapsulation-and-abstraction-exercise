a9e2220cd22eec73d19cbe9fb1de34d492b59b0b
Kent0710 <christian_kent_bayani@dlsl.edu.ph>
2026-08-01 05:01:52 +0800

    feat: add Car.java class that extends Vehicle class

---
+++ Car.java
@@ -0,0 +1,36 @@
+public class Car extends Vehicle {
+
+    private int numberOfSeats;
+
+    public Car(String plateNumber, String model, double baseRatePerDay, boolean available, int numberOfSeats) {
+        super(plateNumber, model, baseRatePerDay, available);
+        setNumberOfSeats(numberOfSeats);
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public final void setNumberOfSeats(int numberOfSeats) {
+        if (numberOfSeats <= 0) {
+            throw new IllegalArgumentException("Number of seats must be greater than 0.");
+        } 
+        // TODO: Add greater than 10 validation check for numberOfSeats
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    @Override
+    public double calculateRentalCost(int numberOfDays) {
+        return getBaseRatePerDay() * numberOfDays;
+    }
+
+    @Override
+    public String getVehicleType() {
+        return "Car";
+    }
+
+    @Override
+    public String getExtraDetail() {
+        return "Seats: " + numberOfSeats;
+    }
+}
